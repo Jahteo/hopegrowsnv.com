448a5f1e258d5df9b18acfa302d00dca9ba38623
Josh Glantz-Hucks <jahteo@gmail.com>
2021-05-10 14:29:46 -0700

    updated newClient.js

--- src/pages/newClient.js
+++ src/pages/newClient.js
@@ -4,105 +4,148 @@ import Layout from '../components/Layout'
 export default function newClient() {
   return (
     <Layout>
-      {/* <h2>Welcome New Clients</h2> */}
-      {/* carousel of images?? */}
+      <article id="main">
+        <header className="special container">
+          {/* <span className="icon solid" /> */}
+          <h2>New Clients</h2>
+        </header>
+        {/* carousel of images?? */}
 
-      {/* instructions/expectations */}
-      <section className="wrapper style1 container special">
-        <div className="row">
-          <div className="col-4 col-12-narrower">
-            <section>
-              <span className="icon solid featured fa-seedling" />
-              <header>
-                <h3>You Do</h3>
-              </header>
-              <p>
-                Fill out the form below with your contact information & submit. There is no commitment at this point, but you have just taken the first steps towards growing into a better you. Well done!
-              </p>
-            </section>
+        {/* instructions/expectations */}
+        <section className="wrapper style1 container special">
+          <div className="row">
+            <div className="col-4 col-12-narrower">
+              <section>
+                <span className="icon solid featured fa-seedling" />
+                <header>
+                  <h3>You Do</h3>
+                </header>
+                <p>
+                  Fill out the form below with your contact information & submit. There is no commitment at this point, but you have just taken the first steps towards growing into a better you. Well done!
+                </p>
+              </section>
+            </div>
+            <div className="col-4 col-12-narrower">
+              <section>
+                <span className="icon solid featured fa-tree" />
+                <header>
+                  <h3>We Do</h3>
+                </header>
+                <p>
+                  We will be reach out to you within 48 hours to schedule your first appointment & send you a custom link to fill out some forms.
+                </p>
+              </section>
+            </div>
+            <div className="col-4 col-12-narrower">
+              <section>
+                <span className="icon solid featured fa-check" />
+                <header>
+                  <h3>While you wait</h3>
+                </header>
+                <p>
+                  Feel free to skim through our resource page or call/email us if you have any urgent concerns.
+                </p>
+              </section>
+            </div>
           </div>
-          <div className="col-4 col-12-narrower">
-            <section>
-              <span className="icon solid featured fa-tree" />
-              <header>
-                <h3>We Do</h3>
-              </header>
-              <p>
-                We will be reach out to you within 48 hours to schedule your first appointment & send you a custom link to fill out some forms.
-              </p>
-            </section>
-          </div>
-          <div className="col-4 col-12-narrower">
-            <section>
-              <span className="icon solid featured fa-check" />
-              <header>
-                <h3>While you wait</h3>
-              </header>
-              <p>
-                Feel free to skim through our resource page or call/email us if you have any urgent concerns.
-              </p>
-            </section>
-          </div>
-        </div>
-      </section>
+        </section>
 
-      {/* form to put in name and number  */}
-      {/* <!-- Feature w colored background  & left sidebar--> */}
-      <section className="wrapper style2 container special-alt">
-        <div className="row gtr-150">
+        {/* form to put in name and number  */}
+        <section className="wrapper style4 special container medium">
 
-          {/* <!-- Content --> */}
-          <div className="col-8 col-12-narrower imp-narrower">
-            <section>
-              <header>
-                <h2>New Client Information</h2>
-              </header>
-              <form target="_blank" action="https://formsubmit.co/1a159c46d07bdfc3e20518b5f9b453dc" method="POST">
-                <div className="form-group">
-                  <input type="hidden" name="_template" value="box" />
-                  {/* <input type="hidden" name="_next" value="https://yourdomain.co/thanks.html" /> */}
-                  <input type="hidden" name="_subject" value="New Client Submission from hopegrowsnv.com!" />
-                  {/* <input type="text" name="_honey" style="display:none"> */}
-                  {/* Add this "honeypot" field to avoid spam by fooling scrapers. If a value is provided, the submission will be silently ignored. The input should be hidden with CSS. Use if they want to disable the reCaptcha */}
-                  <label>
-                    Full Name:
-                    <input type="text" name="new_client_name" className="form-control" placeholder="Full Name" required />
-                  </label>
-                  <br />
-                  <label>
-                    (Guardian's Name, if client is a minor)
-                    <input type="text" name="guardians_name" className="form-control" placeholder="Full Name" />
-                  </label>
-                  <br />
-                  <label>
-                    Email Address:
-                    <input type="email" name="email" className="form-control" placeholder="Email Address" required />
-                  </label>
-                  <br />
-                </div>
-                <button type="submit" className="btn btn-lg btn-dark btn-block">Submit Form</button>
-              </form>
-              {/* <ul className="buttons">
+          <div className="content">
+            <header>
+              <h2>New Client Information</h2>
+            </header>
+            <form target="_blank" action="https://formsubmit.co/1a159c46d07bdfc3e20518b5f9b453dc" method="POST">
+              <input type="hidden" name="_template" value="box" />
+              {/* <input type="hidden" name="_next" value="https://yourdomain.co/thanks.html" /> */}
+              <input type="hidden" name="_subject" value="New Client Submission from hopegrowsnv.com!" />
+              {/* <input type="text" name="_honey" style="display:none"> */}
+              {/* Add this "honeypot" field to avoid spam by fooling scrapers. If a value is provided, the submission will be silently ignored. The input should be hidden with CSS. Use if they want to disable the reCaptcha */}
+              <div className="form-group row gtr-50">
+                <label className="col-6 col-12-mobile">
+                  Client Name:
+                  <input type="text" name="name" placeholder="Full Name" required />
+                </label>
+                <label className="col-6 col-12-mobile">
+                  Guardian's Name (if client is a minor):
+                  <input type="text" name="guardianName" placeholder=" (if applicable)" />
+                </label>
+                <label className="col-6 col-12-mobile">
+                  Email Address:
+                  <input type="email" name="email" placeholder="Email" required />
+                </label>
+                <label className="col-12">
+                  If you have insurance, please let us know who your provider is, otherwise please type "No insurance". (Do not include your plan number here):
+                  <input type="text" name="insurance" placeholder="example: BlueCross Blue Sheild'" required />
+                </label>
+                <label className="col-12">
+                  <ul className="buttons">
+                    <li><input type="submit" className="special" value="Send Message" /></li>
+                  </ul>
+                </label>
+              </div>
+            </form>
+          </div>
+
+        </section>
+
+        {/* <!-- Feature w colored background  & left sidebar--> */}
+        {/* <section className="wrapper style2 container special-alt">
+          <div className="row gtr-150"> */}
+
+        {/* <!-- Content --> */}
+        {/* <div className="col-8 col-12-narrower imp-narrower">
+              <section>
+                <header>
+                  <h2>New Client Information</h2>
+                </header>
+                <form target="_blank" action="https://formsubmit.co/1a159c46d07bdfc3e20518b5f9b453dc" method="POST">
+                  <div className="form-group">
+                    <input type="hidden" name="_template" value="box" /> */}
+        {/* <input type="hidden" name="_next" value="https://yourdomain.co/thanks.html" /> */}
+        {/* <input type="hidden" name="_subject" value="New Client Submission from hopegrowsnv.com!" /> */}
+        {/* <input type="text" name="_honey" style="display:none"> */}
+        {/* Add this "honeypot" field to avoid spam by fooling scrapers. If a value is provided, the submission will be silently ignored. The input should be hidden with CSS. Use if they want to disable the reCaptcha */}
+        {/* <label>
+                      Full Name:
+                      <input type="text" name="new_client_name" className="form-control" placeholder="Full Name" required />
+                    </label>
+                    <br />
+                    <label>
+                      (Guardian's Name, if client is a minor)
+                      <input type="email" name="guardians_name" className="form-control" placeholder="Full Name" />
+                    </label>
+                    <br />
+                    <label>
+                      Email Address:
+                      <input type="text" name="email" className="form-control" placeholder="Email Address" required />
+                    </label>
+                    <br />
+                  </div>
+                  <button type="submit" className="btn btn-lg btn-dark btn-block">Submit Form</button>
+                </form> */}
+        {/* <ul className="buttons">
                 <li><a href="/" className="button small">Submit</a></li>
               </ul> */}
-            </section>
+        {/* </section>
+            </div> */}
+
+        {/* <!-- Sidebar --> */}
+        {/* <div className="col-4 col-12-narrower">
+
+              <section>
+                <header>
+                  <h3>Optional side bar</h3>
+                </header>
+                <p>We can move the instructions (you do, we do, while you wait) down here, or leave this area blank.</p>
+              </section> */}
+
+        {/* </div>
           </div>
-
-          {/* <!-- Sidebar --> */}
-          <div className="col-4 col-12-narrower">
-            {/* <div className="sidebar"> */}
-
-            <section>
-              <header>
-                <h3>Optional side bar</h3>
-              </header>
-              <p>We can move the instructions (you do, we do, while you wait) down here, or leave this area blank.</p>
-            </section>
-
-            {/* </div> */}
-          </div>
-        </div>
-      </section>
+        </section> */}
+      </article>
 
     </Layout>
   )
